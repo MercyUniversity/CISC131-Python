74c3ccfb9b6e9cc855562e11c09b54b69f8f1396
Tianyu W <tianyuelite@gmail.com>
2024-12-03 13:49:58 -0500

    update

--- Unit 11/example_dictionary_methods.py
+++ Unit 11/example_dictionary_methods.py
@@ -4,26 +4,26 @@ book['author'] = 'Tony Gaddis'
 book['price'] = 50
 book['ISBN'] = ['0133862259', '978013386225']
 
-# get() returns a value from a key
-print(book.get('price'))
-# alternatively, using [] operator
-print(book['price'])
+# # get() returns a value from a key
+# print(book.get('price'))
+# # alternatively, using [] operator
+# print(book['price'])
 
-print()
+# print()
 # items() returns all keys and values
-print(book.items())
+# print(book.items())
 
-print()
-# iterate over a dictionary's keys and values
-for item in book.items():
-    print(item)
+# print()
+# # iterate over a dictionary's keys and values
+# for item in book.items():
+#     print(item)
 
-print()
-# iterate over a dictionary's keys only
-for key in book.keys():
-    print(key)
+# print()
+# # iterate over a dictionary's keys only
+# for key in book.keys():
+#     print(key)
     
-print()
-# iterate over a dictionary's values only
+# print()
+# # iterate over a dictionary's values only
 for value in book.values():
     print(value)
--- Unit 11/example_dictionary_to_list.py
+++ Unit 11/example_dictionary_to_list.py
@@ -6,15 +6,15 @@ book['ISBN'] = ['0133862259', '978013386225']
 print(book)
 
 # convert a dictionary to a list of tupples
-items = list(book.items())
-print(items)
+# items = list(book.items())
+# print(items)
 
 # convert a dictionary's keys to a list
-keys = list(book.keys())
-print(keys)
+# keys = list(book.keys())
+# print(keys)
 
-# convert a dictionary's values to a list
+# # convert a dictionary's values to a list
 values = list(book.values())
 print(values)
 
-print()
+# print()
